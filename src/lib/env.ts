96690f78a3bfb3e3daefb6900523265ea87534f1
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2023-09-27 18:43:12 +0300

    Create Frontend

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,6 +3,8 @@ import { EnvType, load } from "ts-dotenv"
 export type Env = EnvType<typeof schema>
 
 export const schema = {
+    NODE_ENV: String,
+    BASE_URL: String,
     DATABASE_URL: String,
     SECRET_KEY: String,
     PORT: Number,
--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,19 @@ import express from "express"
 import router from "./API"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import { createProxyMiddleware } from "http-proxy-middleware"
 
 const app = express()
 const port = env.PORT || 3000
+
+// Serve static files from the Vite development server in development
+if (env.NODE_ENV === "development") {
+    // Proxy requests to the Vite development server
+    app.use("/", createProxyMiddleware({ target: env.BASE_URL, changeOrigin: true }))
+} else {
+    // Serve Vite's static files in production
+    app.use("/", express.static("frontend/dist"))
+}
 
 // Enable cors
 app.use(
