37bd7d9fc013f5ea47bfe967bd8c418ae3964355
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2023-10-03 08:52:59 +0300

    CTA Link to Auth login

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import LandingPageLayout from "../layouts/LandingPageLayout"
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import macbook from "../images/macbook.png"
+import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
@@ -16,16 +17,19 @@ export default function Home() {
                     <h4 className="text-xl lg:text-2xl text-center font-light text-neutral-400">
                         Create, Update, and Showcase Your Delicious Dishes with Ease
                     </h4>
-                    <button className="capitalize w-fit inline-flex items-center gap-6 pl-6 pr-2 py-2 mt-4 text-lg text-white font-semibold bg-orange-500 rounded-full">
+                    <Link
+                        to={"/auth"}
+                        className="capitalize w-fit inline-flex items-center gap-6 pl-6 pr-2 py-2 mt-4 text-lg text-white font-semibold bg-orange-500 rounded-full"
+                    >
                         get started
-                        <div className="w-10 h-10 bg-white text-orange-500 rounded-full relative">
+                        <span className="w-10 h-10 bg-white text-orange-500 rounded-full relative">
                             <FontAwesomeIcon
                                 icon={faArrowRight}
                                 size="lg"
                                 className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"
                             />
-                        </div>
-                    </button>
+                        </span>
+                    </Link>
                 </div>
                 <div className="h-1/2 w-full">
                     <img
